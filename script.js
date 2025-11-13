02b9d5cdaea3a556e90190f549916b651e7c381a
TheHeart <thenikuman2000@gmail.com>
2025-11-13 10:37:15 +0900

    add:javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1. 必要なHTML要素を取得する
+const taskInput = document.getElementById('task-input');
+const addButton = document.getElementById('add-button');
+const taskList = document.getElementById('task-list');
+
+// 2. 要素を取得しているか確認
+console.log(taskInput);
+console.log(addButton);
+console.log(taskList);
